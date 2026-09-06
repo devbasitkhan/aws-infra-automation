2526d89349eaa13cec3e0a52b1192647cba06827
Abdul Basit Khan <abdulbasitkhan.devops@gmail.com>
2026-09-06 14:17:24 +0500

    Remove obsolete nested Terraform outputs

--- OneDrive/Desktop/terraform-project/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.web_server.public_ip
-}
-
-output "rds_endpoint" {
-  description = "Endpoint of the RDS database"
-  value       = aws_db_instance.my_rds.endpoint
-}
